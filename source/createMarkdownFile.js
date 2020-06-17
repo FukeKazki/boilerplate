d87ae3e274bcf4196dfd9a18835c26ef17345835
FukeKazki <kazkichi0906@gmail.com>
2020-06-17 23:24:54 +0900

    Add: 0618

--- source/createMarkdownFile.js
+++ source/createMarkdownFile.js
@@ -1,9 +1,11 @@
 "use strict";
 exports.__esModule = true;
 var fs = require("fs");
-var year = (new Date().getFullYear()).toString();
-var month = (new Date().getMonth() + 1).toString().padStart(2, '0');
-var date = (new Date().getDate()).toString().padStart(2, '0');
+var _a = process.argv, args = _a.slice(2);
+var yyyymmdd = args[0];
+var year = yyyymmdd ? yyyymmdd.slice(0, 4) : (new Date().getFullYear()).toString();
+var month = yyyymmdd ? yyyymmdd.slice(4, 6) : (new Date().getMonth() + 1).toString().padStart(2, '0');
+var date = yyyymmdd ? yyyymmdd.slice(6, 8) : (new Date().getDate()).toString().padStart(2, '0');
 var fileName = "" + year + month + date + ".md";
 var isExist = fs.existsSync(fileName);
 var template = "---\ntitle: ''\ndate: " + year + "-" + month + "-" + date + "\ntags: []\n---\n";
