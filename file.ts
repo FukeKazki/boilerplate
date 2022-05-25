ff219eb9804780220ee176ca4ccc0c77cfc86dd7
Fuke Kazuki <fuke@jig.jp>
2022-05-25 09:39:06 +0900

    Update

--- file.ts
+++ file.ts
@@ -2,7 +2,7 @@
 import { format } from "https://deno.land/std@0.140.0/datetime/mod.ts";
 
 const now = new Date();
-const path = `./source/${format(now, "yyyyMMdd")}.md`
+const path = `./source/${format(now, "yyyyMMdd")}.md`;
 const template = `---
 title: ''
 date: ${format(now, "yyyy-MM-dd")}
@@ -10,8 +10,8 @@ tags: []
 ---`;
 
 try {
-    await Deno.writeTextFile(path, template);
-    console.log("ファイルを作成しました", path);
+  await Deno.writeTextFile(path, template);
+  console.log("ファイルを作成しました", path);
 } catch (err) {
-    console.error("ファイルの作成に失敗しました。", err);
+  console.error("ファイルの作成に失敗しました。", err);
 }
