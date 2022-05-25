9edfadba0139f0b2e8c292c51ae16698db19451b
Fuke Kazuki <fuke@jig.jp>
2022-05-25 09:37:34 +0900

    Update

---
+++ file.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env -S deno run --allow-net --allow-env --allow-write
+import { format } from "https://deno.land/std@0.140.0/datetime/mod.ts";
+
+const now = new Date();
+const path = `./source/${format(now, "yyyyMMdd")}.md`
+const template = `---
+title: ''
+date: ${format(now, "yyyy-MM-dd")}
+tags: []
+---`;
+
+try {
+    await Deno.writeTextFile(path, template);
+    console.log("ファイルを作成しました", path);
+} catch (err) {
+    console.error("ファイルの作成に失敗しました。", err);
+}
