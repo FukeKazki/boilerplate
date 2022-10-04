08b92f54a9affb395dab89a691a8a9b0afbe734e
Fuke Kazuki <fuke@jig.jp>
2022-10-04 14:11:26 +0900

    Update

--- file.ts
+++ file.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env -S deno run --allow-net --allow-env --allow-write
 import { format } from "https://deno.land/std@0.140.0/datetime/mod.ts";
+import { parse } from "https://deno.land/std@0.140.0/flags/mod.ts";
 
+const type = parse(Deno.args).type ?? 'source';
 const now = new Date();
-const path = `./source/${format(now, "yyyyMMdd")}.md`;
+const path = `./${type}/${format(now, "yyyyMMdd")}.md`;
 const template = `---
 title: ''
 date: ${format(now, "yyyy-MM-dd")}
